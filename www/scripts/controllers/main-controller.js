54177a5e0ba17f385f389684c2ed4a9a6775adab
hectorrosell <hrrl@gft.com>
2015-05-18 16:10:37 +0200

    Implement the search in Types Pag and Method pag.

--- www/scripts/controllers/main-controller.js
+++ www/scripts/controllers/main-controller.js
@@ -3,41 +3,18 @@
 app.controller('MainController', ['$rootScope', '$scope', '$log', '$state', '$location', '$route', '$timeout', function ($rootScope, $scope, $log, $state, $location, $route, $timeout) {
 
     $log.debug("MainController");
-
     var currentLocation;
-
     $scope.isSlideBack = false;
-
     $scope.title = "Adaptive Showcase";
-
     $scope.status = statusNextSlice;
-
-
-
     $scope.go = function (location) {
-
-        console.log(location);
-        console.log($state);
-
-        /*if ($state.is("contact")) {
-            $scope.title = "Contact";
-        } else if ($state.is("favorites")) {
-            $scope.title = "Favorites";
-        } else if ($state.is("license")) {
-            $scope.title = "License";
-        } else if ($state.is("services")) {
-            $scope.title = "Services";
-        } else {
-            $scope.title = "Adaptive Showcase";
-        }*/
-
         $state.transitionTo(location);
     }
 
     $scope.isHomePage = function () {
         // console.log("******************************");
         //console.log("Home Page: " + $state.is("home"));
-        if ($state.is("home") || $state.is("units-list") || $state.is("methods-list")) {
+        if ($state.is("units-list") || $state.is("methods-list")) {
             return false;
         } else {
             return true;
@@ -45,65 +22,36 @@ app.controller('MainController', ['$rootScope', '$scope', '$log', '$state', '$lo
     };
 
     $scope.setBackTransitionServices = function () {
-        $log.debug("myFuction1");
-        //$scope.isSlideBack = true;
         $state.transitionTo("home");
 
     };
 
     $scope.setBackTransitionUnits = function () {
-        $log.debug("myFuction2");
-        //$scope.isSlideBack = true;
         $state.transitionTo("units-list");
     };
 
     $scope.setBackTransitionMethods = function () {
-        $log.debug("myFuction3");
-        // $scope.isSlideBack = true;
         $state.transitionTo("methods-list");
     };
 
     if (isFirstState) {
         oldLocation = "home";
         isFirstState = false;
-        $log.debug("isFirstState: " + isFirstState);
     }
 
-    //$rootScope.isHomePage = isHomePage;
-
     $scope.status = statusNextSlice;
-
-    /*$scope.isHomePage = function () {
-        console.log("******************************");
-        console.log("Home Page: " + $state.is("home"));
-        if ($state.is("home") || $state.is("units-list") || $state.is("methods-list")) {
-            return false;
-        } else {
-            return true;
-        }
-    };*/
-
     $scope.scrollItems = dataInfo;
-    //$scope.unit_list = unitList;
-    //$scope.method_list = methodList;
-    //$scope.detail_list = detailList;
+
+
 
     $scope.main_list = dataInfo;
-
     $scope.itemMainListSelected = function (id, location) {
-
-        console.log("location: " + currentLocation);
 
         if (currentLocation.indexOf("uiSidebarLeft") !== -1) {
 
-
         } else {
-            //if (false) {
-
-            //$scope.isSlideBack = false;
             $log.debug("itemMainListSelected");
             var lengthList = dataInfo.length;
-            //$('.slide').addClass('move');
             for (var i = 0; i < lengthList; i++) {
 
                 if (id === $scope.main_list[i].id) {
@@ -113,22 +61,21 @@ app.controller('MainController', ['$rootScope', '$scope', '$log', '$state', '$lo
                     $state.transitionTo('units-list');
                 }
             }
-
         }
     };
 
     $scope.itemUnitListSelected = function (id) {
 
         if (currentLocation.indexOf("uiSidebarLeft") !== -1) {
-
+            console.log('if');
         } else {
-
+            console.log('else');
             $scope.isSlideBack = false;
             statusNextSlice = false;
-            $log.debug("itemUnitListSelected");
             var lengthList2 = unitList.list.length;
-            for (var i2 = 0; i2 < lengthList2; i2++) {
 
+            for (var i2 = 0; i2 < lengthList2; i2++) {
+                console.log('id:' + id + 'unitList.list[i2].id:' + unitList.list[i2].id);
                 if (id === unitList.list[i2].id) {
                     methodList = unitList.list[i2];
                     $scope.method_list = methodList;
@@ -138,6 +85,10 @@ app.controller('MainController', ['$rootScope', '$scope', '$log', '$state', '$lo
             }
         }
     };
+
+    /*$scope.itemUnitListSelected = function (item) {
+        console.log('test: ' + item);
+    }*/
 
     $scope.itemMethodListSelected = function (id) {
 
@@ -161,8 +112,6 @@ app.controller('MainController', ['$rootScope', '$scope', '$log', '$state', '$lo
         }
     };
 
-    // Transitions animation
-
     $scope.$on('$stateChangeSuccess', function () {
 
         if ($state.is("contact")) {
@@ -179,15 +128,7 @@ app.controller('MainController', ['$rootScope', '$scope', '$log', '$state', '$lo
 
     });
 
-
     $scope.$on('$locationChangeStart', function (angularEvent, next, location) {
-
-        /*console.log("locationChangeStart " +
-            " , " + angularEvent +
-            " , " + location +
-            " , " + next
-
-        );*/
 
         var isDownwaids = false;
 
@@ -199,11 +140,11 @@ app.controller('MainController', ['$rootScope', '$scope', '$log', '$state', '$lo
 
             if (newLocation.indexOf(oldLocation) !== -1) {
                 //isDownwaids = true;
-                $log.debug("Is equal state, " + "isDownwards: " + $scope.isSlideBack);
+                //$log.debug("Is equal state, " + "isDownwards: " + $scope.isSlideBack);
                 //$log.debug("Is equal state, " + "isDownwards: " + $scope.isSlideBack);
 
             } else {
-                $log.debug("Is no equal state, " + "isDownwards: " + $scope.isSlideBack);
+                //$log.debug("Is no equal state, " + "isDownwards: " + $scope.isSlideBack);
                 //$log.debug("Is equal state, " + "isDownwards: " + $scope.isSlideBack);
             }
             /*$scope.isDownwards = isDownwards;*/
@@ -226,17 +167,6 @@ app.controller('MainController', ['$rootScope', '$scope', '$log', '$state', '$lo
         //$scope.isSlideBack = false;
         //$rootScope.isDownwards = isDownwaids;
 
-        /*if ($state.is("contact")) {
-            $scope.title = "Contact";
-        } else if ($state.is("favorites")) {
-            $scope.title = "Favorites";
-        } else if ($state.is("license")) {
-            $scope.title = "License";
-        } else if ($state.is("services")) {
-            $scope.title = "Services";
-        } else {
-            $scope.title = "Adaptive Showcase";
-        }*/
     });
 
 }]);
