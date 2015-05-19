1e92428034a3598c2dd0f6165daefc9c5bb31412
hectorrosell <hrrl@gft.com>
2015-05-19 12:29:50 +0200

    Removed transaction, adapted portrait header

--- www/scripts/controllers/main-controller.js
+++ www/scripts/controllers/main-controller.js
@@ -16,7 +16,7 @@ app.controller('MainController', ['$rootScope', '$scope', '$log', '$state', '$lo
     $scope.isHomePage = function () {
         // console.log("******************************");
         //console.log("Home Page: " + $state.is("home"));
-        if ($state.is("units-list") || $state.is("methods-list")) {
+        if ($state.is("home") || $state.is("units-list") || $state.is("methods-list")) {
             return false;
         } else {
             return true;
